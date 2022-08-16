b4126349ecceadef291c00955ce076eeb1cf8b80
Keita Mizukoshi <mizukoshi.keita@jaxa.jp>
2022-08-16 17:53:29 +0900

    Generate csv file

--- README.md
+++ README.md
@@ -3,10 +3,16 @@
 Data logger for keyence FD-X flowmeter
 
 ## requirement
-pyserial - `python -m pip install pyserial`
-USB driver [keyence](keyence.co.jp) if needed.
+ - pyserial - `python -m pip install pyserial`
+ - USB driver [keyence](keyence.co.jp) if needed.
 
 ## Usage 
+To write data,
 ```
 python main.py
 ```
+Then send data from FD-X.
+After sending, kill the script with Ctrl-C.
+`output.csv` will be generated.
+
+The extract notebook is `extract_data.ipynb`
--- main.py
+++ main.py
@@ -3,15 +3,20 @@ from time import sleep
 
 def main():
 
+    out_filename = 'output.csv'
+
     # Edit port for your environment
     port = '/dev/tty.usbserial-0001'
     ser = serial.Serial(port, 9600, timeout=None)
 
+    print('Waiting data...')
     while True:
         line = ser.read_all()
         if line != b'':
-            print(line)
-        sleep(0.01) # second
+            with open(out_filename, 'a') as f:
+                f.write(line.decode('utf-8'))
+
+        sleep(0.01) # unit: second
 
     ser.close()
 
